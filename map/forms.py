9d0263c73a9ccf1890f7f19b9e98af1be817f086
Dmytro-Shvetsov <shvetsovdi2@gmail.com>
2019-09-15 04:22:03 +0300

    Staff admin panel. Departments hierarchy.

--- map/forms.py
+++ map/forms.py
@@ -6,4 +6,4 @@ from bootstrap_modal_forms.forms import BSModalForm
 class MarkerForm(BSModalForm):
     class Meta:
         model = Marker
-        fields = ['name', 'description', 'longitude', 'latitude', 'marker_region', 'image']
+        fields = ['name', 'description', 'longitude', 'latitude', 'marker_region', 'upload_key']
--- map/models.py
+++ map/models.py
@@ -10,14 +10,50 @@ class Region(models.Model):
         return self.name
 
 
+class District(models.Model):
+    name = models.CharField(max_length=100)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    is_district_center = models.BooleanField(default=False)
+    is_region_center = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Town(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Village(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Marker(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000, null=False, blank=False, default='No description given')
+    description = models.TextField(max_length=1000, null=False, blank=False)
     longitude = models.DecimalField(decimal_places=20, max_digits=25, null=True, blank=True)
     latitude = models.DecimalField(decimal_places=20, max_digits=25, null=True, blank=True)
     marker_region = models.ForeignKey(Region, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/')
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    upload_key = models.ImageField(upload_to='images/', null=True, blank=True)
+    # image_storage = models.ForeignKey(ImageStorage, on_delete=models.CASCADE, default=1)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=-1)
+    in_process = models.BooleanField(default=False)
 
     def get_likes_count(self):
         return len(self.markerestimator_set.all().filter(vote=1))
@@ -27,21 +63,22 @@ class Marker(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.image.path)
-        exif = img._getexif()
-        output_size = (350, 350)
-        img.thumbnail(output_size)
-        orientation_key = 274
-        if exif and orientation_key in exif:
-            orientation = exif[orientation_key]
-            rotate_values = {
-                3: Image.ROTATE_180,
-                6: Image.ROTATE_270,
-                8: Image.ROTATE_90
-            }
-            if orientation in rotate_values:
-                img = img.transpose(rotate_values[orientation])
-        img.save(self.image.path)
+        # img = Image.open(self.image.path)
+        #
+        # exif = img._getexif()
+        # output_size = (350, 350)
+        # img.thumbnail(output_size)
+        # orientation_key = 274
+        # if exif and orientation_key in exif:
+        #     orientation = exif[orientation_key]
+        #     rotate_values = {
+        #         3: Image.ROTATE_180,
+        #         6: Image.ROTATE_270,
+        #         8: Image.ROTATE_90
+        #     }
+        #     if orientation in rotate_values:
+        #         img = img.transpose(rotate_values[orientation])
+        # img.save(self.image.path)
 
     def __str__(self):
         return self.name
